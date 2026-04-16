8905714d1927df3dfa5b1069daa838ace1cbcc9d
Aryan Kedare <arykedare@gmail.com>
2026-04-16 23:51:39 +0100

    feat: add SUR (IR blaster) device type as switch
    Implement ZemoteSur entity for IR blaster devices (TV, AC, etc).
    Sends IR command codes via shadow channel on turn_on.

--- custom_components/zemote/switch.py
+++ custom_components/zemote/switch.py
@@ -20,9 +20,15 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     hub: ZemoteHub = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([
-        ZemoteSwitch(hub, d) for d in hub.devices if d.get("platform") == "switch"
-    ])
+    entities = []
+    for d in hub.devices:
+        if d.get("platform") != "switch":
+            continue
+        if d.get("surBrand") or d.get("surCodeset"):
+            entities.append(ZemoteSur(hub, d))
+        else:
+            entities.append(ZemoteSwitch(hub, d))
+    async_add_entities(entities)
 
 
 class ZemoteSwitch(SwitchEntity):
@@ -72,6 +78,62 @@ class ZemoteSwitch(SwitchEntity):
     def turn_on(self, **kwargs: Any) -> None:
         self._state = True
         self._hub.set_channel(self._serial, self._channel, 1)
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs: Any) -> None:
+        self._state = False
+        self._hub.set_channel(self._serial, self._channel, 0)
+        self.schedule_update_ha_state()
+
+
+class ZemoteSur(SwitchEntity):
+    """Zemote IR blaster (SUR) device."""
+
+    def __init__(self, hub: ZemoteHub, device: dict) -> None:
+        self._hub        = hub
+        self._device     = device
+        self._serial     = device["serialNumber"]
+        self._channel    = device["channelKey"]
+        self._attr_name  = device["name"]
+        self._attr_unique_id = device["applianceId"]
+        self._state      = False
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._serial)},
+            name=self._device.get("hubName", self._serial),
+            manufacturer="Contera IoT",
+            model="Zemote Hub",
+        )
+
+    @property
+    def is_on(self) -> bool:
+        return self._state
+
+    async def async_added_to_hass(self) -> None:
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                f"{SIGNAL_STATE_UPDATED}_{self._serial}",
+                self._handle_state_update,
+            )
+        )
+
+    @callback
+    def _handle_state_update(self, reported: dict) -> None:
+        raw = reported.get(self._channel)
+        if raw is not None:
+            self._state = int(raw) != 0
+            self.async_write_ha_state()
+
+    def turn_on(self, **kwargs: Any) -> None:
+        self._state = True
+        self._hub.publish(self._serial, {
+            self._channel: 1,
+            "brand":   self._device.get("surBrand", ""),
+            "codeset": self._device.get("surCodeset", ""),
+        })
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs: Any) -> None:
