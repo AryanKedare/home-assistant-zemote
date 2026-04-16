83cd354c00c38a7ae4a79b34b575f969a83ea0e3
Aryan Kedare <arykedare@gmail.com>
2026-04-16 23:41:26 +0100

    feat: add dimmable light support (1-100% brightness)
    Extend ZemoteLight to handle dimmable channels. Brightness maps
    linearly between HA 0-255 and device 0-100 scale.

--- custom_components/zemote/light.py
+++ custom_components/zemote/light.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.components.light import ColorMode, LightEntity
+from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
@@ -26,7 +26,7 @@ async def async_setup_entry(
 
 
 class ZemoteLight(LightEntity):
-    """Zemote on/off light."""
+    """Zemote dimmable or on/off light."""
 
     def __init__(self, hub: ZemoteHub, device: dict) -> None:
         self._hub        = hub
@@ -35,9 +35,16 @@ class ZemoteLight(LightEntity):
         self._channel    = device["channelKey"]
         self._attr_name  = device["name"]
         self._attr_unique_id = device["applianceId"]
+        self._dimmable   = device.get("dimmable", False)
         self._state      = False
-        self._attr_color_mode            = ColorMode.ONOFF
-        self._attr_supported_color_modes = {ColorMode.ONOFF}
+        self._brightness = 255
+
+        if self._dimmable:
+            self._attr_color_mode            = ColorMode.BRIGHTNESS
+            self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+        else:
+            self._attr_color_mode            = ColorMode.ONOFF
+            self._attr_supported_color_modes = {ColorMode.ONOFF}
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -52,6 +59,10 @@ class ZemoteLight(LightEntity):
     def is_on(self) -> bool:
         return self._state
 
+    @property
+    def brightness(self) -> int | None:
+        return self._brightness if self._dimmable else None
+
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
             async_dispatcher_connect(
@@ -62,21 +73,33 @@ class ZemoteLight(LightEntity):
         )
         val = self._hub.get_channel_state(self._serial, self._channel)
         if val is not None:
-            self._state = int(val) != 0
+            self._apply_value(int(val))
 
     @callback
     def _handle_state_update(self, reported: dict) -> None:
         raw = reported.get(self._channel)
         if raw is not None:
-            self._state = int(raw) != 0
+            self._apply_value(int(raw))
             self.async_write_ha_state()
 
+    def _apply_value(self, value: int) -> None:
+        if value == 0:
+            self._state = False
+            self._brightness = 0
+        else:
+            self._state = True
+            self._brightness = min(255, round(value * 255 / 100))
+
     def turn_on(self, **kwargs: Any) -> None:
-        self._state = True
-        self._hub.set_channel(self._serial, self._channel, 100)
+        if ATTR_BRIGHTNESS in kwargs and self._dimmable:
+            pct = max(1, round(kwargs[ATTR_BRIGHTNESS] * 100 / 255))
+        else:
+            pct = 100
+        self._hub.set_channel(self._serial, self._channel, pct)
+        self._apply_value(pct)
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs: Any) -> None:
-        self._state = False
         self._hub.set_channel(self._serial, self._channel, 0)
+        self._apply_value(0)
         self.schedule_update_ha_state()
